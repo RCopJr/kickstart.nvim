94648b6ce75bc002a9fb6e542e97983f405c495f
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-09-15 10:28:21 -0400

    Prune yazi config

--- lua/custom/plugins/yazi.lua
+++ lua/custom/plugins/yazi.lua
@@ -13,19 +13,19 @@ return {
       '<cmd>Yazi<cr>',
       desc = 'Open yazi at the current file',
     },
-    {
-      -- Open in the current working directory
-      '<leader>cw',
-      mode = { 'n', 'v' },
-      '<cmd>Yazi cwd<cr>',
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      '<c-up>',
-      mode = { 'n', 'v' },
-      '<cmd>Yazi toggle<cr>',
-      desc = 'Resume the last yazi session',
-    },
+    -- {
+    --   -- Open in the current working directory
+    --   '<leader>cw',
+    --   mode = { 'n', 'v' },
+    --   '<cmd>Yazi cwd<cr>',
+    --   desc = "Open the file manager in nvim's working directory",
+    -- },
+    -- {
+    --   '<c-up>',
+    --   mode = { 'n', 'v' },
+    --   '<cmd>Yazi toggle<cr>',
+    --   desc = 'Resume the last yazi session',
+    -- },
   },
   ---@type YaziConfig | {}
   opts = {
