59f552c2a982379e82589e4617aefc66e5fb052e
Ramonito Copreros Jr <ramonito@ontariovirtualschool.ca>
2025-10-03 16:19:52 -0400

    Add github color scheme

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -37,6 +37,15 @@ return {
       vim.api.nvim_set_hl(0, 'TreesitterContextSeparator', { fg = '#555555' })
     end,
   },
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup {}
+    end,
+  },
   -- {
   --   'shaunsingh/nord.nvim',
   --   config = function()
